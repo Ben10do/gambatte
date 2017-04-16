Ben10do/gambatte

commit 11c2de2cba4d9bec64006095e315fe9b61b4e948

Freeze the CPU on Invalid Opcodes
When an invalid opcode is reached (including malformed stop instructions), the CPU will now hang, like an actual Game Boy would.

Note that this commit doubles the cycles necessary to start a stop instruction; this may need rectifying if incorrect.

libgambatte/src/cpu.cpp
@@ -39,6 +39,7 @@ CPU::CPU()
 , h(0x01)
 , l(0x4D)
 , skip_(false)
+, hang_(false)
 {
 }
 
@@ -100,6 +101,7 @@ void CPU::saveState(SaveState &state) {
 	state.cpu.h = h;
 	state.cpu.l = l;
 	state.cpu.skip = skip_;
+	state.cpu.hang = hang_;
 }
 
 void CPU::loadState(SaveState const &state) {
@@ -119,6 +121,7 @@ void CPU::loadState(SaveState const &state) {
 	h = state.cpu.h & 0xFF;
 	l = state.cpu.l & 0xFF;
 	skip_ = state.cpu.skip;
+	hang_ = state.cpu.hang;
 }
 
 // The main reasons for the use of macros is to more conveniently be able to tweak
@@ -496,12 +499,12 @@ void CPU::process(unsigned long const cycles) {
 	while (mem_.isActive()) {
 		unsigned short pc = pc_;
 
-		if (mem_.halted()) {
+		if (mem_.halted() || hang_) {
 			if (cycleCounter < mem_.nextEventTime()) {
 				unsigned long cycles = mem_.nextEventTime() - cycleCounter;
 				cycleCounter += cycles + (-cycles & 3);
 			}
-		} else while (cycleCounter < mem_.nextEventTime()) {
+		} else while (cycleCounter < mem_.nextEventTime() && !hang_) {
 			unsigned char opcode;
 
 			PC_READ(opcode);
@@ -588,7 +591,12 @@ void CPU::process(unsigned long const cycles) {
 				// stop (4 cycles):
 				// Halt CPU and LCD display until button pressed:
 			case 0x10:
-				pc = (pc + 1) & 0xFFFF;
+				unsigned char nextByte;
+				PC_READ(nextByte);
+				if (nextByte != 0x00) {
+					hang_ = true;
+					break;
+				}
 
 				cycleCounter = mem_.stop(cycleCounter);
 
@@ -1679,9 +1687,6 @@ void CPU::process(unsigned long const cycles) {
 
 				break;
 
-			case 0xD3: // not specified. should freeze.
-				break;
-
 				// call nc,nn (24;12 cycles):
 				// Push address of next instruction onto stack and then jump to
 				// address stored in next two bytes in memory, if CF is unset:
@@ -1746,9 +1751,6 @@ void CPU::process(unsigned long const cycles) {
 
 				break;
 
-			case 0xDB: // not specified. should freeze.
-				break;
-
 				// call z,nn (24;12 cycles):
 				// Push address of next instruction onto stack and then jump to
 				// address stored in next two bytes in memory, if CF is set:
@@ -1796,11 +1798,6 @@ void CPU::process(unsigned long const cycles) {
 				FF_WRITE(c, a);
 				break;
 
-			case 0xE3: // not specified. should freeze.
-				break;
-			case 0xE4: // not specified. should freeze.
-				break;
-
 			case 0xE5:
 				push_rr(h, l);
 				break;
@@ -1842,13 +1839,6 @@ void CPU::process(unsigned long const cycles) {
 					WRITE(immh << 8 | imml, a);
 				}
 
-				break;
-
-			case 0xEB: // not specified. should freeze.
-				break;
-			case 0xEC: // not specified. should freeze.
-				break;
-			case 0xED: // not specified. should freeze.
 				break;
 
 			case 0xEE:
@@ -1895,9 +1885,6 @@ void CPU::process(unsigned long const cycles) {
 				// di (4 cycles):
 			case 0xF3:
 				mem_.di();
-				break;
-
-			case 0xF4: // not specified. should freeze.
 				break;
 
 			case 0xF5:
@@ -1961,10 +1948,6 @@ void CPU::process(unsigned long const cycles) {
 				mem_.ei(cycleCounter);
 				break;
 
-			case 0xFC: // not specified. should freeze.
-				break;
-			case 0xFD: // not specified. should freeze
-				break;
 			case 0xFE:
 				{
 					unsigned data;
@@ -1977,6 +1960,10 @@ void CPU::process(unsigned long const cycles) {
 
 			case 0xFF:
 				rst_n(0x38);
+				break;
+				
+			default: // Invalid opcode; freeze the CPU.
+				hang_ = true;
 				break;
 			}
 		}

libgambatte/src/cpu.h
@@ -79,6 +79,7 @@ private:
 	unsigned hf1, hf2, zf, cf;
 	unsigned char a_, b, c, d, e, /*f,*/ h, l;
 	bool skip_;
+	bool hang_;
 
 	void process(unsigned long cycles);
 };

libgambatte/src/initstate.cpp
@@ -1179,6 +1179,7 @@ void gambatte::setInitState(SaveState &state, bool const cgb, bool const gbaCgbM
 	state.cpu.h = 0x01;
 	state.cpu.l = 0x4D;
 	state.cpu.skip = false;
+	state.cpu.hang = false;
 
 	std::memset(state.mem.sram.ptr, 0xFF, state.mem.sram.size());
 

libgambatte/src/savestate.h
@@ -55,6 +55,7 @@ struct SaveState {
 		unsigned char h;
 		unsigned char l;
 		unsigned char /*bool*/ skip;
+		unsigned char /*bool*/ hang;
 	} cpu;
 
 	struct Mem {

libgambatte/src/statesaver.cpp
@@ -223,6 +223,7 @@ SaverList::SaverList() {
 	{ static char const label[] = { h,             NUL }; ADD(cpu.h); }
 	{ static char const label[] = { l,             NUL }; ADD(cpu.l); }
 	{ static char const label[] = { s,k,i,p,       NUL }; ADD(cpu.skip); }
+	{ static char const label[] = { h,a,n,g,       NUL }; ADD(cpu.hang); }
 	{ static char const label[] = { h,a,l,t,       NUL }; ADD(mem.halted); }
 	{ static char const label[] = { v,r,a,m,       NUL }; ADDPTR(mem.vram); }
 	{ static char const label[] = { s,r,a,m,       NUL }; ADDPTR(mem.sram); }

commit 1e8ba86fae2807fbff4f96a44c4f6ab01cef91d5

Fix issues that caused tests to fail
This corrects the cycles needed for a stop instruction (broken in 11c2de2), and does the correct byte swapping when checking framebuffers (to account for cb3de24).

Plus:
- Flush stdout after every test, to give a visual indication of progress.
- Return an error code of 1 if any tests fail.
- Don’t run the tests immediately after make-ing them

Makefile
@@ -118,7 +118,6 @@ test: $(TEST)
 		test/hwtests/*/*.asm \
 		test/hwtests/*/*/*.asm \
 		test/hwtests/*/*/*/*.asm
-	$(TEST) $(TEST_GBS)
 
 PNG_LFLAGS != $(PKG_CONFIG) --libs libpng
 $(TEST): $(TEST_OBJECTS) $(LIB)

libgambatte/src/cpu.cpp
@@ -593,6 +593,7 @@ void CPU::process(unsigned long const cycles) {
 			case 0x10:
 				unsigned char nextByte;
 				PC_READ(nextByte);
+				cycleCounter -= 4;
 				if (nextByte != 0x00) {
 					hang_ = true;
 					break;

test/testrunner.cpp
@@ -53,9 +53,9 @@ static void readPng(gambatte::uint_least32_t out[], std::FILE &file) {
 
 	for (std::size_t y = 0; y < gb_height; ++y)
 	for (std::size_t x = 0; x < gb_width; ++x) {
-		out[y * gb_width + x] = rows[y][x * 4] << 16
+		out[y * gb_width + x] = rows[y][x * 4]
 			| rows[y][x * 4 + 1] << 8
-			| rows[y][x * 4 + 2];
+			| rows[y][x * 4 + 2] << 16;
 	}
 }
 
@@ -285,6 +285,7 @@ static void runTestRom(
 	}
 
 	std::putchar(gb.isCgb() ? 'c' : 'd');
+	std::fflush(stdout);
 
 	long samplesLeft = samples_per_frame * 15;
 
@@ -375,4 +376,5 @@ int main(int const argc, char *argv[]) {
 
 	std::printf("\n\nRan %d tests.\n", numTestsRun);
 	std::printf("%d failures.\n", numTestsRun - numTestsSucceeded);
+	return numTestsRun - numTestsSucceeded != 0;
 }